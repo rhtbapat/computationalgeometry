af17a962c526ac5004312075b3ce6e7e3222f499
bapatrohitvijay <bapatrohitvijay@gmail.com>
2016-12-13 14:55:14 +0530

    Vector Angle

--- CG/CGVector.js
+++ CG/CGVector.js
@@ -5,14 +5,19 @@ var CGVector = (function () {
     
     var magnitude;
     var unitVector;
+    var vectorAngXY;
 
     function CGVector(x1,y1,z1) {
         this.x = parseFloat(x1);
         this.y = parseFloat(y1);
         this.z = parseFloat(z1);
         
-        this.magnitude = Math.sqrt(x*x + y*y +z*z);
+        this.magnitude = Math.sqrt(x1*x1 + y1*y1 +z1*z1);
         this.unitVector = new CGVector(x1/this.magnitude, y1/this.magnitude, z1/this.magnitude);
+        this.vectorAngXY = Math.atan2(y1,x1);
+        while(this.vectorAngXY <= -Math.PI){
+            this.vectorAngXY += 2*Math.PI
+        }
     }
     
     CGVector.prototype.getMagnitude = function () {
