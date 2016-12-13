1844713c385c9a6cdfa6d93dc08dc5d4bc187a45
bapatrohitvijay <bapatrohitvijay@gmail.com>
2016-12-13 15:15:15 +0530

    Line length

--- CG/CGLine.js
+++ CG/CGLine.js
@@ -6,15 +6,18 @@ var CGLine = (function () {
     var faces = [];
     var vec;
     var normalVec;
+    var length;
 
     function CGLine(x1,y1,z1,x2,y2,z2) {
         this.point1 = new CGPoint(x1,y1,z1);
         this.point2 = new CGPoint(x2,y2,z2);
+        this.createVectors();
     }
     
     CGLine.prototype.createVectors() {
         this.vec = new CGVector(this.point2.x-this.point1.x, this.point2.y-this.point1.y, this.point2.z-this.point1.z);
         this.normalVec = this.vec.getUnitVector();
+        this.length = this.vec.magnitude;
     }
     
     CGLine.prototype.getFaces = function () {
