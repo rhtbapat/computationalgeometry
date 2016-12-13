f7f5a7abc71fd2415167bad9350945b4d5f1fd66
bapatrohitvijay <bapatrohitvijay@gmail.com>
2016-12-13 12:26:21 +0530

    Create CGVector.js
    Initial Commit

---
+++ CG/CGVector.js
@@ -0,0 +1,24 @@
+var CGVector = (function () {
+    var x;
+    var y;
+    var z;
+    
+    var magnitude;
+    var unitVector;
+
+    function CGVector(x1,y1,z1) {
+        this.x = parseFloat(x1);
+        this.y = parseFloat(y1);
+        this.z = parseFloat(z1);
+        
+        this.magnitude = Math.sqrt(x*x + y*y +z*z);
+        this.unitVector = new CGVector(x1/this.magnitude, y1/this.magnitude, z1/this.magnitude);
+    }
+    
+    CGVector.prototype.getMagnitude = function () {
+        return this.magnitude;
+    }
+
+    return CGVector;
+
+})();
